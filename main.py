edd386a3aa0344ce3a964d0b96bef361ad2496a9
kennyleung-su <kenleung@stanford.edu>
2017-05-24 22:47:38 -0700

    Adds vocabulary initialization scripts.

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import os
 import tensorflow as tf
 import numpy as np
 
+from var.utils.vocab import Vocab
+
 flags = tf.app.flags
 
 flags.DEFINE_string('model', '', 'The name of the model to run.')
-flags.DEFINE_string('data_dir', 'data/', 'The directory containing data files.')
+flags.DEFINE_string('data_dir', 'var/data/', 'The directory containing data files.')
 flags.DEFINE_string('mode', 'train', 'Running mode: either "train", "dev", or "test."')
+flags.DEFINE_string('vocab_file', 'vocab.txt', 'The file containing a list of vocab.')
 
 # TODO: Add hyperparameter flags
 
@@ -31,6 +34,10 @@ def test():
 
 
 def main(unused_argv):
+
+    # Load the vocabulary file.
+    vocab = Vocab(FLAGS.vocab_file, os.path.join(FLAGS.data_dir, 'essays/dev'))
+
     with tf.Graph().as_default():
         # TODO: Build the specified model and run it on the specified mode.
         raise NotImplementedError
