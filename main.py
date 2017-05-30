3c999c3efb6f4af67549fbb68c1ad9b78b38048d
kennyleung-su <kenleung@stanford.edu>
2017-05-29 18:29:11 -0700

    Rnn changes, reducing embedding dimensionality.

--- main.py
+++ main.py
@@ -22,10 +22,11 @@ flags.DEFINE_string('input_type', 'essays', 'Input data feature type: either "es
 flags.DEFINE_string('preprocessor', 'tokenized', 'Name of directory with processed essay files.')
 flags.DEFINE_string('ngram_lengths', '0,2,3,4', 'Comma-separated list of n-gram sizes to use as features.')
 
-# Model hyperparameters
-flags.DEFINE_string('max_seq_len', 10000, 'Max number of words in an example.')
-flags.DEFINE_string('batch_size', 100, 'Number of examples to run in a batch.')
-flags.DEFINE_string('num_epochs', 10, 'Number of epochs to train for.')
+flags.DEFINE_integer('max_seq_len', 10000, 'Max number of words in an example.')
+flags.DEFINE_integer('batch_size', 100, 'Number of examples to run in a batch.')
+flags.DEFINE_integer('num_epochs', 10, 'Number of epochs to train for.')
+flags.DEFINE_integer('embedding_size', 16, 'Size of trainable embeddings, applicable for char-gram embedding models.')
+flags.DEFINE_integer('hidden_size', 100, 'Hidden size of RNN cells, applicable for neural net models.')
 
 # Training and testing
 flags.DEFINE_string('train_split', 'train', 'Split to train the model on.')
@@ -112,8 +113,12 @@ def get_model(vocab, dataset):
         'max_seq_len': vocab.size(), # size of dataset
         'num_classes': len(dataset.CLASS_LABELS)
     }
-    # TODO: Returns the correct corresponding model. Let's start with just SVM.
-    return LinearSvmModel(vocab, **kwargs)
+    if FLAGS.model == 'baseline':
+        return LinearSvmModel(vocab, **kwargs)
+    elif FLAGS.model == 'rnn':
+        return RnnModel(vocab, FLAGS.embedding_size, FLAGS.hidden_size, **kwargs)
+    else:
+        raise ValueError("Unrecognized model: %s." % FLAGS.model)
 
 
 def main(unused_argv):
---
+++ src/models/rnn.py
@@ -0,0 +1,66 @@
+import numpy as np
+import tensorflow as tf
+
+from model import NativeLanguageIdentificationModel
+
+class RnnModel(NativeLanguageIdentificationModel):
+    """A model that trains an RNN classifier on character ngram inputs."""
+
+    def __init__(self, vocab, embedding_size, hidden_size, *args, **kwargs):
+        super(RnnModel, self).__init__(*args, **kwargs)
+        self._vocab = vocab
+        self._embedding_size = embedding_size
+        self._hidden_size = hidden_size
+
+
+    def add_prediction_op(self):
+        with tf.variable_scope('prediction'):
+            # Initialize embeddings, with shape [vocab_size x hidden_size]
+            # TODO: add regularizer to all trainable variables
+            embeddings = tf.get_variable('embeddings',
+                shape=(self._vocab.size(), self._embedding_size),
+                initializer=tf.contrib.layers.xavier_initializer(),  # TODO: consider different initializers
+                dtype=tf.float64
+            )
+
+            embedded_inputs = tf.nn.embedding_lookup(embeddings, self.essay_inputs_placeholder)
+
+            cell = tf.contrib.rnn.LSTMCell(
+                self._hidden_size,
+                initializer=tf.contrib.layers.xavier_initializer()  # TODO: consider different initializers
+            )
+
+            projected_embedding_inputs = tf.layers.dense(embedded_inputs,
+                self._hidden_size,
+                kernel_initializer=tf.contrib.layers.xavier_initializer(),  # TODO: consider different initializers
+                name="prem_proj")
+
+            # TODO: apply sequence length, or else the RNN will loop over padded indices
+            outputs, _ = tf.nn.dynamic_rnn(cell, embedded_inputs, dtype=tf.float64)
+
+            # TODO: capture final state for variable-length sequences
+            final_state = outputs[:,-1,:]
+
+            # First layer
+            # TODO: Add more layers, and add kernel_regularizer using l2_regularization.
+            h1 = tf.layers.dense(final_state,
+                self._num_classes,
+                kernel_initializer=tf.contrib.layers.xavier_initializer(),
+                activation=tf.tanh,
+                use_bias=False,
+                name='h1')
+
+            # Second layer
+            # h2 = tf.layers.dense(h1,
+            #                      self._num_classes,
+            #                      kernel_initializer=tf.contrib.layers.xavier_initializer(),
+            #                      # kernel_regularizer=tf.contrib.layers.l2_regularizer(self._l2_reg),
+            #                      activation=tf.tanh,
+            #                      use_bias=False,
+            #                      name='h2')
+
+            # Final softmax activation
+            logits = tf.nn.softmax(h1, name='logits')
+            preds = tf.argmax(logits, axis=1)
+
+            return preds, logits
