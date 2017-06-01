618c02b5343816054de8acbaad1f6656568c3b69
Kenny Leung <kennyleungsf@gmail.com>
2017-06-01 23:27:25 +0000

    Fixed arglen error with batching (added length placeholder for eval).

--- src/models/model.py
+++ src/models/model.py
@@ -104,7 +104,7 @@ class NativeLanguageIdentificationModel(object):
 
 
     def evaluate_on_batch(self, sess, essay_inputs_batch, essay_inputs_len_batch, labels_batch):
-        feed = self.create_feed_dict(labels_batch, essay_inputs_batch)
+        feed = self.create_feed_dict(labels_batch, essay_inputs_batch, essay_inputs_len_batch)
         accuracy, loss, predictions = sess.run([self.acc_op, self.loss, self.pred], feed)
         return accuracy, loss, predictions
 
