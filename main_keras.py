6697418e6a2c9c6e0f78af3f11eff21913f3d499
Roger Chen <rogerthechen@gmail.com>
2017-06-01 22:26:46 -0700

    snapshot

--- main_keras.py
+++ main_keras.py
@@ -1,32 +1,17 @@
-import os
-
+import os 
 import numpy as np
 import pandas as pd
 
 from keras.layers import Dense, Dropout, Activation
 from keras.models import Sequential
-from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.text import Tokenizer, one_hot
 from keras.utils import to_categorical
+from keras.wrappers.scikit_learn import KerasClassifier
 
 from nltk import ngrams
 
-# CLASS_LABELS = [
-#     'ARA', 'CHI', 'FRE', 'GER', 'HIN', 'ITA', 'JPN', 'KOR', 'SPA', 'TEL', 'TUR'
-# ]
-
-CLASS_LABELS = {
-    'ARA': 0,
-    'CHI': 1,
-    'FRE': 2,
-    'GER': 3,
-    'HIN': 4,
-    'ITA': 5,
-    'JPN': 6,
-    'KOR': 7,
-    'SPA': 8,
-    'TEL': 9,
-    'TUR': 10
-}
+from sklearn.model_selection import cross_val_score, KFold
+from sklearn.preprocessing import LabelEncoder
 
 BATCH_SIZE = 100
 NUM_WORDS = 1000
@@ -37,46 +22,68 @@ def load_files_from_directory(path):
         if filename.endswith(".txt"):
             contents = open("%s/%s" % (path, filename)).read()
             data.append(contents)
-        else:
-            continue
     return np.array(data)
+
 
 def load_labels_from_file(path):
     labels = pd.read_csv(path)
-    return [CLASS_LABELS[i] for i in np.concatenate(labels.as_matrix()[:, [3]]).ravel()]
+    return np.concatenate(labels.as_matrix()[:, [3]]).ravel()
+
 
 print "Loading data..."
 x_train = load_files_from_directory("var/data/essays/train/tokenized")
 x_dev = load_files_from_directory("var/data/essays/dev/tokenized")
-y_train = load_labels_from_file("var/data/labels/train/labels.train.csv")
-y_dev = load_labels_from_file("var/data/labels/dev/labels.dev.csv")
-
-NUM_CLASSES = len(np.unique(y_train))
+y_train_raw = load_labels_from_file("var/data/labels/train/labels.train.csv")
+y_dev_raw = load_labels_from_file("var/data/labels/dev/labels.dev.csv")
 
 print "Vectorizing sequence data..."
+
 tokenizer = Tokenizer(num_words=NUM_WORDS, split=" ")
 tokenizer.fit_on_texts(x_train)
-x_train = tokenizer.sequences_to_matrix(x_train, mode="freq")
-x_dev = tokenizer.sequences_to_matrix(x_dev, mode="freq")
+
+x_train = tokenizer.texts_to_matrix(x_train, mode="tfidf")
+x_dev = tokenizer.texts_to_matrix(x_dev, mode="tfidf")
+
+print x_train[0]
 
 print "Convert class vector..."
-y_train = to_categorical(y_train, NUM_CLASSES)
-y_dev = to_categorical(y_dev, NUM_CLASSES)
+encoder = LabelEncoder()
+encoder.fit(y_train_raw)
+
+y_train_encoded = encoder.transform(y_train_raw)
+y_dev_encoded = encoder.transform(y_dev_raw)
+
+y_train_dummy = to_categorical(y_train_encoded)
+y_dev_dummy = to_categorical(y_dev_encoded)
+
+# basically randomly guessing
+def baseline_model():
+    model = Sequential()
+    model.add(Dense(4, input_shape=(1000,), kernel_initializer='normal', activation='relu'))
+    model.add(Dense(11, kernel_initializer='normal', activation='sigmoid'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+# doing ok
+def my_first_model():
+    model = Sequential()
+    model.add(Dense(units=100, input_dim=NUM_WORDS))
+    # model.add(Activation('relu'))
+    # model.add(Dropout(0.5))
+    model.add(Dense(units=11))
+    model.add(Activation('softmax'))
+
+    model.compile(loss='categorical_crossentropy',
+                optimizer='adam',
+                metrics=['accuracy'])
+    return model
 
 print "Building model..."
-model = Sequential()
-model.add(Dense(units=64, input_shape=(NUM_WORDS,)))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(units=NUM_CLASSES))
-model.add(Activation('softmax'))
 
-model.compile(loss='categorical_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
+model = my_first_model()
 
-history = model.fit(x_train, y_train,
+history = model.fit(x_train, y_train_dummy,
                     batch_size=BATCH_SIZE,
-                    epochs=10,
+                    epochs=200,
                     verbose=1,
-                    validation_data=(x_dev, y_dev))
+                    validation_data=(x_dev, y_dev_dummy))
