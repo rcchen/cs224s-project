447364cc3e9b19d6fbe9dcbebfbd9e62bda152db
kennyleung-su <kenleung@stanford.edu>
2017-06-03 23:38:24 -0700

    Adds in all data - speech transcriptions, ivectors. Be sure to delete old pkl files.

--- src/models/model.py
+++ src/models/model.py
@@ -13,7 +13,9 @@ class NativeLanguageIdentificationModel(object):
         self._l2_reg = l2_reg
 
 
-    def create_feed_dict(self, labels_batch, essay_inputs_batch, essay_inputs_lengths):
+    def create_feed_dict(self, labels_batch, essay_inputs_batch, essay_inputs_len_batch,
+        speech_transcription_inputs_batch, speech_transcriptions_inputs_len_batch,
+        ivector_inputs_batch):
         """Creates the feed_dict for one step of training.
         A feed_dict takes the form of:
         feed_dict = {
@@ -25,10 +27,10 @@ class NativeLanguageIdentificationModel(object):
 
         feed_dict[self.labels_placeholder] = labels_batch
         feed_dict[self.essay_inputs_placeholder] = essay_inputs_batch
-        feed_dict[self.essay_inputs_lengths] = essay_inputs_lengths
-        # TODO: Add in transcriptions, ivectors.
-        # feed_dict[self.speech_transcriptions_inputs_placeholder] = speech_transcription_inputs_batch
-        # feed_dict[self.ivector_inputs_placeholder] = ivector_inputs_batch
+        feed_dict[self.essay_inputs_lengths] = essay_inputs_len_batch
+        feed_dict[self.speech_transcriptions_inputs_placeholder] = speech_transcription_inputs_batch
+        feed_dict[self.speech_transcriptions_inputs_lengths] = speech_transcriptions_inputs_len_batch
+        feed_dict[self.ivector_inputs_placeholder] = ivector_inputs_batch
 
         return feed_dict
 
@@ -46,11 +48,12 @@ class NativeLanguageIdentificationModel(object):
         self.essay_inputs_lengths = tf.placeholder(tf.int32, shape=(None), name='essay_input_lengths')
         self.essay_inputs_placeholder = tf.placeholder(tf.int64, \
             shape=(None, self._max_seq_len), name='essay_inputs')
-        # self.speech_transcriptions_inputs_placeholder = tf.placeholder(tf.int64, \
-        #     shape=(None, self._max_seq_len), name='speech_transcription_inputs')
-        # TODO: Replace `None` with ivector dimension size
-        # self.ivector_inputs_placeholder = tf.placeholder(tf.int64, \
-        #     shape=(None, None), name='ivector_inputs')
+        self.speech_transcriptions_inputs_placeholder = tf.placeholder(tf.int64, \
+            shape=(None, self._max_seq_len), name='speech_transcription_inputs')
+        self.speech_transcriptions_inputs_lengths = tf.placeholder(
+            tf.int32, shape=(None), name='speech_transcription_input_lengths')
+        self.ivector_inputs_placeholder = tf.placeholder(tf.int64, \
+            shape=(None, 800), name='ivector_inputs')
 
 
     def add_prediction_op(self):
@@ -97,14 +100,24 @@ class NativeLanguageIdentificationModel(object):
         return tf.train.AdamOptimizer().minimize(loss)
 
 
-    def train_on_batch(self, sess, essay_inputs_batch, essay_inputs_len_batch, labels_batch):
-        feed = self.create_feed_dict(labels_batch, essay_inputs_batch, essay_inputs_len_batch)
+    def train_on_batch(self, sess, essay_inputs_batch, essay_inputs_len_batch,
+                       speech_transcription_inputs_batch, speech_transcriptions_inputs_len_batch,
+                       ivector_inputs_batch, labels_batch):
+        feed = self.create_feed_dict(labels_batch, essay_inputs_batch, essay_inputs_len_batch,
+                                     speech_transcription_inputs_batch,
+                                     speech_transcriptions_inputs_len_batch,
+                                     ivector_inputs_batch)
         _, loss, summary = sess.run([self.train_op, self.loss, self.summary_op], feed)
         return loss, summary
 
 
-    def evaluate_on_batch(self, sess, essay_inputs_batch, essay_inputs_len_batch, labels_batch):
-        feed = self.create_feed_dict(labels_batch, essay_inputs_batch, essay_inputs_len_batch)
+    def evaluate_on_batch(self, sess, essay_inputs_batch, essay_inputs_len_batch,
+                          speech_transcription_inputs_batch, speech_transcriptions_inputs_len_batch,
+                          ivector_inputs_batch, labels_batch):
+        feed = self.create_feed_dict(labels_batch, essay_inputs_batch, essay_inputs_len_batch,
+                                     speech_transcription_inputs_batch,
+                                     speech_transcriptions_inputs_len_batch,
+                                     ivector_inputs_batch)
         accuracy, loss, predictions = sess.run([self.acc_op, self.loss, self.pred], feed)
         return accuracy, loss, predictions
 
