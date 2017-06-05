c846814d94a6b6207a61d3ee8d04d36ec5924eb1
kennyleung-su <kenleung@stanford.edu>
2017-06-04 23:29:04 -0700

    Major changes to replace old POS sequence method with POS ngrams.

--- src/models/model.py
+++ src/models/model.py
@@ -4,12 +4,13 @@ class NativeLanguageIdentificationModel(object):
     """Abstracts a Tensorflow graph for the NLI task."""
 
     def __init__(self, batch_size, max_seq_len, num_classes, l2_reg, vocab,
-                 embedding_size, hidden_size):
+                 embedding_size, hidden_size, pos_vocab):
         self._batch_size = batch_size
         self._max_seq_len = max_seq_len
         self._num_classes = num_classes
         self._l2_reg = l2_reg
         self._vocab = vocab
+        self._pos_vocab = pos_vocab
         # GloVe vector data
         self._embedding_size = embedding_size
         self._hidden_size = hidden_size
